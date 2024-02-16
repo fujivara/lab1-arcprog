366f8569f2db818ed1bd921815e657a699fd2570
fujivara <marat.skyba@gmail.com>
2024-02-16 19:42:08 +0200

    Create server

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC3339)
+	response := TimeResponse{Time: currentTime}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/time", fooHandler)
+	log.Fatal(http.ListenAndServe(":9875", nil))
+}
