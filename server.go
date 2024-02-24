2a03eba719fd5fd143e918c0134132248e0660a5
fujivara <marat.skyba@gmail.com>
2024-02-24 16:15:32 +0200

    Still keep going

--- server.go
+++ server.go
@@ -22,5 +22,6 @@ func fooHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/time", fooHandler)
 	// go go go go
+	// go go go go
 	log.Fatal(http.ListenAndServe(":9875", nil))
 }
