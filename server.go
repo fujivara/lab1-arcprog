83c15b16703c329891d36e6d38a046e440cad3ad
fujivara <marat.skyba@gmail.com>
2024-02-24 15:53:52 +0200

    Added go go go go

--- server.go
+++ server.go
@@ -21,6 +21,6 @@ func fooHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/time", fooHandler)
-
+	// go go go go
 	log.Fatal(http.ListenAndServe(":9875", nil))
 }
